ec1580432e12e08fc715304d83e52a899f184e89
VaioJun <1014gin@Gmail.com>
2017-01-31 15:38:56 -0500

    Proto of LOGIN PAGE

---
+++ app/src/main/java/com/example/vaiojun/login_4d/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.vaiojun.login_4d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+/**
+ * Created by VaioJun on 01/31/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
